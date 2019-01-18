ebb3c9ff8db47ad4bdb9d3ade5b4ab4f7ffa6c5e
Jacob Smith <jsmith0361@jh-mba158-19.local>
2019-01-18 11:44:45 -0600

    added section of images

--- rockPaperScissors/ViewController.swift
+++ rockPaperScissors/ViewController.swift
@@ -10,11 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var topLeftHand: UIImageView!
+    @IBOutlet weak var topRightHand: UIImageView!
+    var random = Int.random(in: 0...2)
+    var random2 = Int.random(in: 0...2)
+    @IBOutlet var EnemyPlayerHand: [UIImageView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    
+    func determineWinner() {
+        var win: Bool = false
+        for hand in EnemyPlayerHand {
+            if hand.image == UIImage(named: "rock") {
+                topLeftHand
+            }
+        }
     }
 
 
+    @IBAction func whenPlayPressed(_ sender: UIButton) {
+        
+        var hand: UIImage!
+        switch random {
+        case 0:
+            topLeftHand.image = UIImage(named: "rock")
+            hand = topLeftHand.image
+        case 1:
+            topLeftHand.image = UIImage(named: "scissors")
+            hand = topLeftHand.image
+        case 2:
+            topLeftHand.image = UIImage(named: "paper")
+            hand = topLeftHand.image
+        default:
+            return
+        }
+        
+        var hand2: UIImage!
+        switch random2 {
+        case 0:
+            topRightHand.image = UIImage(named: "rock")
+            hand2 = topRightHand.image
+        case 1:
+            topRightHand.image = UIImage(named: "scissors")
+            hand2 = topRightHand.image
+        case 2:
+            topRightHand.image = UIImage(named: "paper")
+            hand2 = topRightHand.image
+        default:
+            return
+        }
+        
+    }
+    
 }
 
